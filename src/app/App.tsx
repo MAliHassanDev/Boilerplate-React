2e9ba52dc90dc017af7caf5f580396ff5dc98632
ali <muhammadalihassanofficial@gmail.com>
2024-12-24 10:48:27 +0500

    style: format code with prettier

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import styles from "./App.module.css";
-import SwitchTheme from '@/components/switchTheme/SwitchTheme';
-
+import SwitchTheme from "@/components/switchTheme/SwitchTheme";
 
 const App = () => {
   return (
